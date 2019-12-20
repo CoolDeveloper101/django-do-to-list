e4346deaaa66f51d2150404cf06999395a448566
CoolDeveloper101 <guptaashwin.12345@gmail.com>
2019-12-20 17:25:17 +0530

    Adding some task models

--- main/models.py
+++ main/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    task = models.CharField(max_length=200)
+    date_started = models.DateTimeField()
+
+    def __str__(self):
+        return self.task
+
+
+class DoneTask(models.Model):
+    task = models.CharField(max_length=200)
+    date_started = models.DateTimeField()
+    date_done = models.DateTimeField()
