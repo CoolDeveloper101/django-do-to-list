239c401cedc802377da30dbbfb6769a5216234bd
CoolDeveloper101 <guptaashwin.12345@gmail.com>
2019-12-21 13:10:09 +0530

    Adding the urls to add a todo and delete a todo

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='homepage'),
+    path('add_todo/', views.add_todo, name='add_todo'),
+    path('delete_todo/<int:id>', views.delete_todo, name='delete_todo'),
 ]
