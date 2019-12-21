e5c923a36af809cd6c0dbb5291bc45cd4794c435
CoolDeveloper101 <guptaashwin.12345@gmail.com>
2019-12-21 13:22:40 +0530

    Adding the views to handle the request

--- main/urls.py
+++ main/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='homepage'),
     path('add_todo/', views.add_todo, name='add_todo'),
-    path('delete_todo/<int:id>', views.delete_todo, name='delete_todo'),
+    path('delete_todo/<int:todo_id>', views.delete_todo, name='delete_todo'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from .models import Task, DoneTask
+from django.utils import timezone
 
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>This is testing</h1>")
+
+def add_todo(request):
+    if request.method == 'POST':
+        t = request.POST['content']
+        current = timezone.now()
+        Task.objects.create(task=t, date_started=current)
+
+    return redirect('homepage')
+
+def delete_todo(request, todo_id):
+    if request.method == 'POST':
+        tasks = Task.objects.all()
+        tasks.get(id=todo_id).delete()
+
+    return redirect('homepage')
